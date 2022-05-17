441ed07ba42342eaeb9974fc65520b5ce8375332
Pickle99 <jackrestler@gmail.com>
2022-05-17 20:48:17 +0400

    chore: install spatie translatable package

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,8 @@ return new class() extends Migration {
 	{
 		Schema::create('users', function (Blueprint $table) {
 			$table->id();
-			$table->string('name');
-			$table->string('email')->unique();
+			$table->json('username');
+			$table->json('email');
 			$table->timestamp('email_verified_at')->nullable();
 			$table->string('password');
 			$table->rememberToken();
