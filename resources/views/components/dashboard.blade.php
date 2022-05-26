908bd6338c9978a5cb2125e3c457fd6965cd5e1e
Pickle99 <jackrestler@gmail.com>
2022-05-27 02:00:16 +0400

    style: add responsive for dashboard worldwide page

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -2,11 +2,22 @@
 <header class="border-b-2 h-20 flex items-center justify-between">
         <img class="ml-4 w-30 h-10 lg:ml-52 h-12 lg:w-auto" src="{{asset('storage/images/coronalogo.png')}}" alt="Workflow">
        <div class="flex items-center">
-       <x-dropdown></x-dropdown>
+       <x-dropdown class="mr-5 lg:mr-0"></x-dropdown>
 
           <h1 class="lg:ml-10 hidden lg:block font-extrabold text-black">{{auth()->user()->username}}</h1>
-
-           <div class="mx-4 border-r-4"></div>
+           <span class="mr-5 block lg:hidden group">
+              <img class="" src="{{asset('storage/images/drop.png')}}" alt="dropdown"/>
+              <div class="absolute top-16 right-4 hidden group-hover:block">
+                  <a class="lg:ml-10  font-extrabold text-black">{{auth()->user()->username}}</a>
+                 <div class="items-center">
+                 <form method="POST" action="/logout">
+                     @csrf
+                     <button class="text-sm " type="submit"> {{__('translate.logout')}}</button>
+                 </form>
+             </div>
+              </div>
+          </span>
+           <div class="lg:mx-4 border-r-4"></div>
 
              <div class="mt-4 items-center hidden lg:block">
                  <form method="POST" action="/logout">
@@ -20,8 +31,8 @@
     <div class="mx-4 mt-10 lg:mx-52 lg:mt-10">
         <h1 class="text-black text-2xl font-bold lg:text-3xl">{{__('translate.worldwide_stats')}}</h1>
         <div class="flex mt-10 h-10 border-b-2">
-                <a href="/dashboard?page=worldwide&{{http_build_query(request()->except('page'))}}" class="{{$page === 'worldwide' || is_null($page) ? 'font-bold border-b-4 border-black text-lg lg:text-sm lg:text-xl mr-20' : 'text-xl mr-20'}}" >{{__('translate.worldwide')}}</a>
-                <a href="/dashboard?page=country&{{http_build_query(request()->except('page'))}}"  class="{{$page === 'country' ? 'font-bold border-b-4 border-black text-lg lg:text-xl mr-20' : 'text-lg lg:text-xl mr-20'}}" >{{__('translate.by_country')}}</a>
+                <a href="/dashboard?page=worldwide&{{http_build_query(request()->except('page'))}}" class="{{$page === 'worldwide' || is_null($page) ? 'font-bold border-b-4 border-black text-lg lg:text-sm lg:text-xl mr-20' : 'text-xl mr-20'}}" ><span class="block">{{__('translate.worldwide')}}</span></a>
+            <a href="/dashboard?page=country&{{http_build_query(request()->except('page'))}}"  class="{{$page === 'country' ? 'w-fit font-bold border-b-4 border-black text-lg lg:text-xl mr-20' : 'text-lg lg:text-xl mr-20'}}" ><span class="block w-40">{{__('translate.by_country')}}</span></a>
             </div>
     </div>
     <div>
--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,5 +1,4 @@
-
-<button class="relative inline-block text-left group">
+<button {{$attributes->merge(['class' => "relative inline-block text-left group"])}}>
     <div>
         <div type="button" class="inline-flex  justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="menu-button" aria-expanded="true" aria-haspopup="true">
             @if(app()->getLocale() === 'en')
