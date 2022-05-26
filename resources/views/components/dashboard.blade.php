2107f89cdde32a3ea3bdafd4441d2431510198e6
Pickle99 <jackrestler@gmail.com>
2022-05-27 01:20:48 +0400

    wip: responsive design for dashboard worldwide page

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -1,27 +1,27 @@
 <x-layout>
 <header class="border-b-2 h-20 flex items-center justify-between">
-        <img class="ml-52 h-12 w-auto" src="{{asset('storage/images/coronalogo.png')}}" alt="Workflow">
+        <img class="ml-4 w-30 h-10 lg:ml-52 h-12 lg:w-auto" src="{{asset('storage/images/coronalogo.png')}}" alt="Workflow">
        <div class="flex items-center">
        <x-dropdown></x-dropdown>
 
-          <h1 class="ml-10 font-extrabold text-black">{{auth()->user()->username}}</h1>
+          <h1 class="lg:ml-10 hidden lg:block font-extrabold text-black">{{auth()->user()->username}}</h1>
 
-           <div class="mx-5 border-r-4"></div>
+           <div class="mx-4 border-r-4"></div>
 
-             <div class="mt-4 items-center">
+             <div class="mt-4 items-center hidden lg:block">
                  <form method="POST" action="/logout">
                      @csrf
-                     <button class="mr-52 text-sm " type="submit"> {{__('translate.logout')}}</button>
+                     <button class="mr-4 lg:mr-52 text-sm " type="submit"> {{__('translate.logout')}}</button>
                  </form>
              </div>
 
        </div>
 </header>
-    <div class="mx-52 mt-10">
-        <h1 class="font-extrabold text-black text-3xl">{{__('translate.worldwide_stats')}}</h1>
+    <div class="mx-4 mt-10 lg:mx-52 lg:mt-10">
+        <h1 class="text-black text-2xl font-bold lg:text-3xl">{{__('translate.worldwide_stats')}}</h1>
         <div class="flex mt-10 h-10 border-b-2">
-                <a href="/dashboard?page=worldwide&{{http_build_query(request()->except('page'))}}" class="{{$page === 'worldwide' || is_null($page) ? 'font-bold border-b-4 border-black text-xl mr-20' : 'text-xl mr-20'}}" >{{__('translate.worldwide')}}</a>
-                <a href="/dashboard?page=country&{{http_build_query(request()->except('page'))}}"  class="{{$page === 'country' ? 'font-bold border-b-4 border-black text-xl mr-20' : 'text-xl mr-20'}}" >{{__('translate.by_country')}}</a>
+                <a href="/dashboard?page=worldwide&{{http_build_query(request()->except('page'))}}" class="{{$page === 'worldwide' || is_null($page) ? 'font-bold border-b-4 border-black text-lg lg:text-sm lg:text-xl mr-20' : 'text-xl mr-20'}}" >{{__('translate.worldwide')}}</a>
+                <a href="/dashboard?page=country&{{http_build_query(request()->except('page'))}}"  class="{{$page === 'country' ? 'font-bold border-b-4 border-black text-lg lg:text-xl mr-20' : 'text-lg lg:text-xl mr-20'}}" >{{__('translate.by_country')}}</a>
             </div>
     </div>
     <div>
--- resources/views/components/worldwide.blade.php
+++ resources/views/components/worldwide.blade.php
@@ -1,18 +1,22 @@
 
-<div class="flex justify-center">
-    <div class="px-36 py-10 flex flex-col items-center  bg-opacity-0.08   mt-10 bg-newcases rounded-2xl shadow-custombox w-fit">
+<div class="flex flex-col items-center lg:justify-center lg:flex lg:flex-row">
+    <div class="px-20 py-10 lg:px-36 lg:py-10 flex flex-col items-center  bg-opacity-0.08   mt-10 bg-newcases rounded-2xl shadow-custombox">
         <img src="{{asset('/storage/images/blue.png')}}" alt="new cases" />
         <p class="my-5 text-2xl ">{{__('translate.new_cases')}}</p>
-        <p class="mt-2 font-extrabold text-5xl">{{$confirmed}}</p>
+        <p class="mt-2 text-newcases font-extrabold text-4xl lg:text-5xl">{{$confirmed}}</p>
     </div>
-    <div class="px-36 py-10 flex flex-col items-center  bg-opacity-0.08  mx-20 mt-10 bg-greener rounded-2xl shadow-custombox w-fit">
-        <img src="{{asset('/storage/images/green.png')}}" alt="new cases" />
+
+<div class="flex lg:flex-row">
+    <div class="px-6 py-10 mr-4 lg:px-36 lg:py-10 flex flex-col items-center  bg-opacity-0.08  lg:mx-20 mt-10 bg-greener rounded-2xl shadow-custombox">
+        <img class="w-24 h-10" src="{{asset('/storage/images/green.png')}}" alt="new cases" />
         <p class="my-5 text-2xl ">{{__('translate.recovered')}}</p>
-        <p class="mt-2 font-extrabold text-5xl">{{$recovered}}</p>
+        <p class="mt-2 text-greener font-extrabold text-4xl lg:text-5xl">{{$recovered}}</p>
     </div>
-    <div class="px-36 py-10 flex flex-col items-center  bg-opacity-0.08   mt-10 bg-yellowb rounded-2xl shadow-custombox w-fit">
-        <img src="{{asset('/storage/images/yellow.png')}}" alt="new cases" />
+    <div class="px-6 py-10 lg:px-36 lg:py-10 flex flex-col items-center  bg-opacity-0.08   mt-10 bg-yellowb rounded-2xl shadow-custombox">
+        <img class="w-24 h-10" src="{{asset('/storage/images/yellow.png')}}" alt="new cases" />
         <p class="my-5 text-2xl ">{{__('translate.death')}}</p>
-        <p class="mt-2 font-extrabold text-5xl">{{$deaths}}</p>
+        <p class="text-yellowb mt-2 font-extrabold text-4xl lg:text-5xl">{{$deaths}}</p>
     </div>
 </div>
+</div>
+
