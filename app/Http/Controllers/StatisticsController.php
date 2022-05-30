7c40b54023b4558d14fcffa6761f68d5f34c2bae
Pickle99 <jackrestler@gmail.com>
2022-05-31 01:40:03 +0400

    refactor: make code for sorting more clean

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Country;
 use App\Models\Info;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class StatisticsController extends Controller
@@ -11,62 +12,17 @@ class StatisticsController extends Controller
 	/*
 	Show country statistics
 	*/
-	public function index(): View
+	public function index(Request $request): View
 	{
-		$countries = Country::latest()->filter(request(['search']))->get();
+		$sortBy = $request->sortBy ?? 'created_at';
+		$sortDirection = $request->sortDirection === 'asc' ? 'asc' : 'desc';
+		$countries = Country::orderBy(Info::select($sortBy)->whereColumn('infos.country_id', 'countries.id'), $sortDirection)
+			->latest()->filter(request(['search']))->get();
 		$infos = Info::all();
 		$page = request('page');
 		$confirmed = Info::sum('confirmed');
 		$recovered = Info::sum('recovered');
 		$deaths = Info::sum('deaths');
-
-		//for by country name sorting
-		if (request()->get('sort') === 'country_desc')
-		{
-			$countries = Country::orderBy(Info::select('country')->whereColumn('infos.country_id', 'countries.id'), 'desc')
-					->latest()->filter(request(['search']))->get();
-		}
-		if (request()->get('sort') === 'country_asc')
-		{
-			$countries = Country::orderBy(Info::select('country')->whereColumn('infos.country_id', 'countries.id'), 'asc')
-					->latest()->filter(request(['search']))->get();
-		}
-
-		//   for new cases sorting
-		if (request()->get('sort') === 'cases_desc')
-		{
-			$countries = Country::orderBy(Info::select('confirmed')->whereColumn('infos.country_id', 'countries.id'), 'desc')
-					->latest()->filter(request(['search']))->get();
-		}
-		if (request()->get('sort') === 'cases_asc')
-		{
-			$countries = Country::orderBy(Info::select('confirmed')->whereColumn('infos.country_id', 'countries.id'), 'asc')
-					->latest()->filter(request(['search']))->get();
-		}
-
-		// for deaths sorting
-		if (request()->get('sort') === 'deaths_desc')
-		{
-			$countries = Country::orderBy(Info::select('deaths')->whereColumn('infos.country_id', 'countries.id'), 'desc')
-					->latest()->filter(request(['search']))->get();
-		}
-		if (request()->get('sort') === 'deaths_asc')
-		{
-			$countries = Country::orderBy(Info::select('deaths')->whereColumn('infos.country_id', 'countries.id'), 'asc')
-					->latest()->filter(request(['search']))->get();
-		}
-
-		//for recovered sorting
-		if (request()->get('sort') === 'recovered_desc')
-		{
-			$countries = Country::orderBy(Info::select('recovered')->whereColumn('infos.country_id', 'countries.id'), 'desc')
-					->latest()->filter(request(['search']))->get();
-		}
-		if (request()->get('sort') === 'recovered_asc')
-		{
-			$countries = Country::orderBy(Info::select('recovered')->whereColumn('infos.country_id', 'countries.id'), 'asc')
-					->latest()->filter(request(['search']))->get();
-		}
 
 		return view('components.dashboard', [
 			'page'          => $page,
