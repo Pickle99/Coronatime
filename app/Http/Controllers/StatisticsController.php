f2d19451ef7be594c54a004b78cc310bce729cad
Pickle99 <jackrestler@gmail.com>
2022-06-01 13:12:12 +0400

    refactor: inline some variables in statistics controller

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -18,19 +18,14 @@ class StatisticsController extends Controller
 		$sortDirection = $request->sortDirection === 'asc' ? 'asc' : 'desc';
 		$countries = Country::orderBy(Info::select($sortBy)->whereColumn('infos.country_id', 'countries.id'), $sortDirection)
 			->latest()->filter(request(['search']))->get();
-		$infos = Info::all();
-		$page = request('page');
-		$confirmed = Info::sum('confirmed');
-		$recovered = Info::sum('recovered');
-		$deaths = Info::sum('deaths');
 
 		return view('components.dashboard', [
-			'page'          => $page,
+			'page'          => request('page'),
 			'countries'     => $countries,
-			'infos'         => $infos,
-			'confirmed'     => number_format($confirmed),
-			'recovered'     => number_format($recovered),
-			'deaths'        => number_format($deaths),
+			'infos'         => Info::all(),
+			'confirmed'     => Info::sum('confirmed'),
+			'recovered'     => Info::sum('recovered'),
+			'deaths'        => Info::sum('deaths'),
 		]);
 	}
 }
